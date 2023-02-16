c2017a438fa20af020e4ee63749f0f0ca0d4be49
B N SHIVADATTA KUMAR <84439168+BNSHIVADATTAKUMAR@users.noreply.github.com>
2023-02-16 13:41:54 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run PES1UG20CS100\n";
-  cout << "PES1UG20CS100\n";
+  cout << "I have successfully built and run PES1UG20CS100.this has been edited.\n";
   return 0;
 }
